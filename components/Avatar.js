57429dd1d4f43250d378c4583ae45c6bb07c140b
Lucas Mazzutti <lucasmazzutti@MacBook-Pro-de-Lucas.local>
2021-08-02 23:28:33 +0200

    adding index and search pages and basic functionality

---
+++ components/Avatar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Avatar = ({url, className}) => {
+    return (
+        <img
+            loading='lazy'
+            src={url}
+            alt="profile_pic"
+            className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
+        />
+    )
+}
+
+export default Avatar;
